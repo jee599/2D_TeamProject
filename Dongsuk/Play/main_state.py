70047323d31c08fb4ad27cdb83836d822acf2288
지동석 <jee599@naver.com>
2017-10-18 05:35:39 +0900

    TP.Jump
    dongsuk

--- Dongsuk/Play/main_state.py
+++ Dongsuk/Play/main_state.py
@@ -14,6 +14,10 @@ boy = None
 grass = None
 font = None
 
+def handle_events():
+    global running
+    events = get_events()
+
 class Back:
     def __init__(self):
         self.image = load_image('Resource/Prison.png')
@@ -30,19 +34,35 @@ class Grass:
         self.image.clip_draw(self.frame *100, 0, 800, 70 ,400, 30)
 
 class Boy:
+
     def __init__(self):
-        self.x, self.y = 50, 90
+        self.x, self.y = 50, 95
         self.frame = 0
+        self.time = 0
         self.image = load_image('Resource/animation_sheet.png')
         self.dir = 1
+        self.state = 0
+
+    def handle_event(self, event):
+
+        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP):
+            if self.state == 0:
+                self.state = 1
+                self.time = 0
 
     def update(self):
         self.frame = (self.frame + 1) % 8
-        if self.x >= 800:
-            self.dir = -1
-        elif self.x <= 0:
-            self.dir = 1
+        self.time = self.time + 1
 
+        if self.y < 95:
+            self.y = 95
+            self.state = 0
+
+        if self.state == 1:
+            self.y = self.y + 20 - 9.8*self.time/5
+            if self.y < 95:
+                self.y = 95
+                self.state = 0
     def draw(self):
         self.image.clip_draw((self.frame * 100), 0, 100, 100, self.x, self.y)
 
@@ -72,6 +92,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.chage_state(title_state)
+        else:
+            boy.handle_event(event)
 
     pass
 

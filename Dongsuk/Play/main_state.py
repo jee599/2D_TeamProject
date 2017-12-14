a9545b61d18c80ee787e97451459f6e19bd0ea52
지동석 <jee599@naver.com>
2017-12-15 03:46:06 +0900

    update
    22

--- Dongsuk/Play/main_state.py
+++ Dongsuk/Play/main_state.py
@@ -26,7 +26,7 @@ def handle_events():
 class Life:
     def __init__(self):
         self.image = load_image('Resource/nomlife.png')
-        self.switch = 5
+        self.switch = 1
     def draw(self):
         global stage
         if(stage == 0):
@@ -48,48 +48,14 @@ class Background:
     image1 = None
     def __init__(self):
         if Background.image == None:
-            Background.image = load_image('Resource/back1.png')
+            Background.image = load_image('Resource/back00.png')
         if Background.image == None:
-            Background.image = load_image('Resource/back2.png')
+            Background.image1 = load_image('Resource/back2.png')
         self.frame = 0
-        self.timer = 0
-        self.limit = 10
     def update(self):
-        self.timer += 1
-        if self.timer > self.limit*4:
-            self.timer = 0
-        if self.timer < self.limit:
-            self.frame = 0
-        elif self.timer < self.limit*2:
-            self.frame = 1
-        elif self.timer < self.limit*3:
-            self.frame = 2
-        else:
-            self.frame = 3
+        self.frame = (self.frame + 1)%8
     def draw(self):
         self.image.clip_draw((self.frame*800), 0, 800,600, 400, 300)
-class Background2:
-    SCROLL_SPEED_PPS = 200
-    image = None
-    def __init__(self):
-        if Background2.image == None:
-            Background2.image = load_image('Resource/skull7.png')
-        self.x = 500
-        self.frame = 1
-        self.left = 0
-        self.speed = 3
-        self.screen_width = 800
-        self.screen_height = 600
-        pass
-
-    def draw(self):
-        x = int(self.left)
-        w = min(Background2.image.w - x, self.screen_width)
-        Background2.image.clip_draw_to_origin(x, 0, w, self.screen_height, 0, 0)
-        Background2.image.clip_draw_to_origin(0, 0, self.screen_width - w, self.screen_height, w, 0)
-
-    def update(self):
-        self.left = (self.left + self.frame * self.speed) % self.image.w
 
 class Wall:
     def __init__(self):
@@ -103,24 +69,27 @@ class Object:
         self.image = load_image('Resource/bat1.png')
         self.frame = 0
         self.x = random.randint(600,800)
-        self.speed = random.randint(7,20)
-        self.y = random.randint(50,500)
+        self.speed = random.randint(7,13)
+        self.y = random.randint(100,200)
 
     def get_box(self):
-        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+        return self.x - 15, self.y - 15, self.x + 15, self.y + 15
 
     def draw_bb(self):
         draw_rectangle(*self.get_box())
 
     def new(self):
-        self.x = 800
-        self.speed = random.randint(3, 15)
-        self.y = random.randint(50, 500)
+        self.x += 800
+        self.speed = random.randint(7, 13)
+        self.y = random.randint(100, 200)
 
     def update(self):
         self.frame = (self.frame + 1) % 6
         self.x = self.x - self.speed
-
+        if self.frame < 4:
+            self.y -= 3
+        else:
+            self.y += 3
         if self.x <  20:
             self.x = 800
             self.speed = random.randint(3,15)
@@ -129,51 +98,21 @@ class Object:
         self.image.clip_draw(self.frame * 100, 0, 90, 90, self.x, self.y)
 class Object2:
     def __init__(self):
-        self.image = load_image('Resource/bat1.png')
-        self.frame = 0
-        self.x = random.randint(600,800)
-        self.speed = random.randint(7,17)
-        self.y = random.randint(50,500)
+        self.image = load_image('Resource/horn1.png')
+        self.x = 600 + random.randint(100,1000)
+        self.y = 70
 
     def get_box(self):
-        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+        return self.x - 5, self.y, self.x + 20, self.y + 25
 
     def draw_bb(self):
         draw_rectangle(*self.get_box())
-
+    def new(self):
+        self.x += random.randint(600,1000)
     def update(self):
-        self.frame = (self.frame + 1)%6
-        self.x = self.x - self.speed
-        if self.x <  50:
-            self.x = 800
-            self.speed = random.randint(3,15)
-            self.y = random.randint(50,500)
+        self.x = self.x - 7
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 90, 90, self.x, self.y)
-
-class Grass:
-    SCROLL_SPEED_PPS = 200
-    image = None
-    def __init__(self):
-        if Grass.image == None:
-            Grass.image = load_image('Resource/tile2.png')
-        self.x = 200
-        self.frame = 1
-        self.left = 0
-        self.speed = 20
-        self.screen_width = 800
-        self.screen_height = 75
-        pass
-
-    def draw(self):
-        x = int(self.left)
-        w = min(Grass.image.w - x, self.screen_width)
-        Grass.image.clip_draw_to_origin(x, 0, w, self.screen_height, 0, 0)
-        Grass.image.clip_draw_to_origin(0, 0, self.screen_width - w, self.screen_height, w, 0)
-
-    def update(self):
-        self.frame = 1
-        self.left = (self.left + self.frame * self.speed) % self.image.w
+        self.image.clip_draw(0, 0, 90, 90, self.x, self.y)
 
 def collide(a,b):
     left_a, bottom_a, right_a, top_a = a.get_box()
@@ -192,7 +131,6 @@ class Boy:
         self.frame = 0
         self.time = 0
         self.time2 = 0
-        #self.image1 = load_image('Resource/animation_sheet2.png)
         self.image = load_image('Resource/animation_sheet1.png')
         self.dir = 1
         self.state = 0
@@ -221,12 +159,12 @@ class Boy:
             self.y = 70
             self.state = 0
         if self.state == 1:
-            self.y = self.y + 30 - 9.8*self.time/2
+            self.y = self.y + 35 - 9*self.time/2
             if self.y < 70:
                 self.y = 70
                 self.state = 0
         if self.state == 2:
-            self.y = self.y + 30 - 9.8*self.time2/2
+            self.y = self.y + 35 - 9*self.time2/2
             if self.y < 70 :
                 self.y = 70
                 self.state = 0
@@ -235,29 +173,28 @@ class Boy:
             self.image.clip_draw((self.frame * 100), 0, 100, 100, self.x, self.y)
 
 def enter():
-    global boy, wall, back, team, back1, life, bgm, font
+    global boy, wall, back, team,life, bgm, font, team1
     boy = Boy()
     life = Life()
     wall = Wall()
     back = Background()
-    #back1 = Background2()
-    team = [Object() for i in range(6)]
-
-    bgm=load_music('Nomplay.mp3')
+    team1 = [Object2() for i in range(2)]
+    team = [Object() for i in range(3)]
+    bgm = load_music('Nomplay.mp3')
     bgm.set_volume(64)
     bgm.repeat_play()
     pass
 
 def exit():
-    global boy, wall, back, back1, life, team,bgm,font
+    global boy, wall, back, life, team,bgm,team1
     del(boy)
     del(bgm)
-    del(font)
     del(team)
     del(life)
     del(wall)
     del(back)
-    del(back1)
+    del(team1)
+    score = 0
     pass
 
 def pause():
@@ -267,13 +204,13 @@ def resume():
     pass
 
 def handle_events():
-    global Box
+    global Box, life, game_framework
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.chage_state(title_state)
+            game_framework.change_state(title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_TAB:
             if Box == 0:
                 Box = 1
@@ -285,39 +222,52 @@ def handle_events():
     pass
 
 def update():
-    global score, font, bgm, Box
+    global score, font, bgm, Box, game_framework, life, boy
+
+    if life.switch < 1:
+        game_framework.quit()
 
     if Box == 1:
         score += 1
-    else :
+    else:
         score += 2
     boy.update()
     back.update()
-    #back1.update()
+    for Object2 in team1:
+        Object2.update()
     for Object in team:
         Object.update()
     pass
 
 def draw():
-    global score,font,bgm,ife,Box
+    global score,font,bgm,Box
 
     clear_canvas()
     back.draw()
-    #back1.draw()
     wall.draw()
     life.draw()
     boy.draw()
+    for Object2 in team1:
+        if collide(boy,Object2) == False:
+            Object2.draw()
+            if Box == 1:
+                Object2.draw_bb()
+        else:
+            if life.switch != 0:
+                life.switch -= 1
+            Object2.new()
     if Box == 1:
         boy.draw_bb()
-
     if font == None:
         font = load_font('Resource\ENCR10B.TTF')
-    if(Box == 1):
+    if (Box == 1):
         font.draw(350,450 ,'Easy')
     else:
         font.draw(350,450, 'Hard')
-    font.draw(350,375,'Stage : %d' % stage)
+
+    font.draw(350,375,'Stage : %d' % (stage + 1))
     font.draw(350,350, 'Score : %d' % score)
+
     for Object in team:
         if collide(boy,Object) == False:
             Object.draw()
@@ -328,5 +278,5 @@ def draw():
                 life.switch -= 1
             Object.new()
     update_canvas()
-    delay(0.03 + Box*0.03)
+    delay(0.06 + Box*0.02)
     pass
